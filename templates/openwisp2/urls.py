2e91d5bd6b4ac617408ef28227a340989e536b7c
DimPolissiou <dimpolissiou@gmail.com>
2017-03-06 14:16:00 +0200

    Update urls.py

--- templates/openwisp2/urls.py
+++ templates/openwisp2/urls.py
@@ -5,7 +5,14 @@ from django_netjsonconfig.admin_theme.admin import admin, openwisp_admin
 openwisp_admin()
 
 urlpatterns = [
+    url(r'^$', RedirectView.as_view(pattern_name='devices_home', permanent=True), name='index'),
+    url(r'^devices/', include('devices.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^accounts/login$', login, name='cas_ng_login'),
+    url(r'^accounts/logout$', logout, name='cas_ng_logout'),
+    url(r'^accounts/callback$', callback, name='cas_ng_proxy_callback'),
+    url(r'^collectd_rest/', include('collectd_rest.urls')),
+    
     # controller URLs
     # used by devices to download/update their configuration
     # keep the namespace argument unchanged
